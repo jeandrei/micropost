7e11081f51ae3ac84557036f19e3f1cf82727bae
jeandrei <jeandreiwalter@gmail.com>
2022-08-31 17:01:58 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+
+const mongoose = require('mongoose');
+
+//mongo:27017 mongo é o nome do container
+mongoose.connect('mongodb://mongo:27017/nodeApp',{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log("CONNECTION OPEN!!!");
+})
+.catch(err => {
+    console.log("OH NO ERROR!!!")
+    console.log(err);
+})
+
+
+app.get('/', (req, res) => {
+    res.send("SISTEMA CARREGADO");
+})
+
+
+app.listen(3000, () => {
+    console.log("Liste on port 3000");
+})
